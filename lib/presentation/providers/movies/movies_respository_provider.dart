39bd63b2c6a888efee445988dbab4377c764af6e
HarryOreLara <harryorelara@gmail.com>
2023-08-07 22:04:35 -0500

    Solo una clase en la noche y estudiarla

---
+++ lib/presentation/providers/movies/movies_respository_provider.dart
@@ -0,0 +1,10 @@
+import 'package:cinemapedia/infraestructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infraestructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+
+//Este repositorio es inmutable de solo lectura
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl(MovieDbDatasource());
+});
